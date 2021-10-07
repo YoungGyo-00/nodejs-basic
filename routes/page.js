d6fdb53f8c1120c2b0eaaecc19b655cd79e25dd6
younggyo <lee4519698@hanmail.net>
2021-10-07 14:56:41 +0000

    middlewares router 추가 및 연결

--- routes/page.js
+++ routes/page.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
 router.use((req,res,next) => {
-	res.locals.user = null;
+	res.locals.user = req.user; // passport deserializeUser 요철 => req.session에 저장된 id로 db 조회
 	res.locals.followerCount = 0;
 	res.locals.followingCount = 0;
 	res.locals.followerIdList = [];
 });
 
-router.get('/profile', (req,res) =>{
+router.get('/profile', isLoggedIn, (req,res) =>{
 	res.render('profile', { title: '내 정보 - NodeBird'});
-});
+}); // 로그인 상태에서만 실행
 
-router.get('/join',(req,res) =>{
+router.get('/join', isNotLoggedIn, (req,res) =>{
 	res.render('join',{title: '회원가입'});
-});
+}); // 로그인 안된 상태에서만 실행
 
 router.get('/',(req,res) =>{
 	const twits = [];
