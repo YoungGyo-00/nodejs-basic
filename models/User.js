c042347db03bca478b2d1cfdf37b391e670d9aa0
younggyo <lee4519698@hanmail.net>
2021-10-07 13:56:05 +0000

    User associated 수정

--- models/User.js
+++ models/User.js
@@ -38,6 +38,16 @@ module.exports = class User extends Sequelize.Model{
 	}
 	
 	static associate(db){
-		
+		db.User.hasMany(db.Post);
+		db.User.belongsToMany(db.User,{
+			foreignKey: 'followingId',
+			as: 'Followers',
+			through: 'Follow',
+		});
+		db.User.belongsToMany(db.User,{
+			foreignKey: 'followerId',
+			as: 'followings',
+			through: 'Follow',
+		});
 	}
 };
